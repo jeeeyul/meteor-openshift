173ad8cf994f898a2716dcffe84b8fafcb902a48
Jeeeyul Lee <jeeeyul@gmail.com>
2016-04-21 15:28:07 +0900

    update dns

--- meteorshim.js
+++ meteorshim.js
@@ -1,12 +1,14 @@
 #!/bin/env node
 
 // Setup env
-process.env.ROOT_URL = "http://"+process.env.OPENSHIFT_APP_DNS || "http://localhost";
+var APP_DNS = process.env.OPENSHIFT_APP_DNS_ALIAS || process.env.OPENSHIFT_APP_DNS;
+
+process.env.ROOT_URL = "http://" + APP_DNS || "http://localhost";
 process.env.MONGO_URL = process.env.MONGODB_URL || "PLEASE_PROVIDE_A_MONGO_URL";
 // process.env.MAIL_URL = "smtp://email:password@smtp.gmail.com:465";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-process.env.DDP_DEFAULT_CONNECTION_URL = 'http://' + process.env.OPENSHIFT_APP_DNS + ':8000';
+process.env.DDP_DEFAULT_CONNECTION_URL = 'http://' + APP_DNS + ':8000';
 
 // Show connection details on startup
 console.log("*********** Meteor 1.3 Env ************");
